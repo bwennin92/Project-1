67c40e8fb6930d2d6f6a50ead25d6409f0a060c3
bwennin92 <brandonmw1@hotmail.com>
2022-05-06 18:37:32 -0700

    Re doing javascript. got power checkbox added

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-let playerStreak = [];
-let order = [];
-let turn;
-let complete;
+let playerTurn = [];
+let intervalId;
+let round;
+let good;
 let compTurn;
-let sectionId;
-let reset = false;
-let difficultHard = false;
-let victory;
+let blink;
+let win;
+let power = false;
+
+
 const streakCounter = document.querySelector("#turn");
 const topLeft = document.querySelector("#topleft");
 const topRight = document.querySelector("#topright");
@@ -15,40 +16,19 @@ const bottomRight = document.querySelector("#bottomright");
 const extCircle = document.querySelector("#extcircle");
 const playerBox = document.querySelector("#playerbox");
 const startButton = document.querySelector("#play");
+const powerButton = document.querySelector('#power')
 const resetButton = document.querySelector("#reset");
-const difficultyButton = document.querySelector("#hardmode")
-// compTurn = true;
+const difficultyButton = document.querySelector("#hardmode");
+let order = [topLeft, topRight, bottomLeft, bottomRight];
 
-
-function gameOn(){
-    victory = false;
-    order =[topLeft,topRight,bottomLeft,bottomRight];
-    playerStreak = [];
-    blink = 0;
-    sectionId = 0;
-    turn = 1;
-    streakCounter.innerHTML = 1;
-    complete = true;
+powerButton.addEventListener('click', (e) => {
+if(powerButton.checked == true) {
+    power = true;
+    streakCounter.innerHTML = "-";
+    alert('Power is on!');
+}else {
+    if(powerButton.checked == false)
+    streakCounter.innerHTML = '';
+    alert("Powering down :(")
 }
-
-let blink = panel => {
-    return new Promise((resolve, reject) =>{
-        panel.className += 'active';
-        setTimeout(() =>{
-            panel.className = panel.className.replace('active', '');
-        }, 500)
-    })
-}
-
-
-
-
-startButton.addEventListener('click', (e) =>{
-        streakCounter.innerText = '-';
-        gameOn()
 })
-resetButton.addEventListener('click', (e) =>{
-    streakCounter.innerText =''
-})
-
-
