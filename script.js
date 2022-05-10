1ec683c88e1cd55d330537f1b5c15978cd8c52e7
bwennin92 <brandonmw1@hotmail.com>
2022-05-09 18:48:53 -0700

    updated random color function

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let playerTurn = [];
-let intervalId;
 let round;
 let good;
 let blink;
 let win;
 let power = false;
+let compBlink = [];
 
+const allColors = document.querySelectorAll(".colors");
 
 const streakCounter = document.querySelector("#turn");
 const topLeft = document.querySelector("#topleft");
@@ -15,53 +16,80 @@ const bottomRight = document.querySelector("#bottomright");
 const extCircle = document.querySelector("#extcircle");
 const playerBox = document.querySelector("#playerbox");
 const startButton = document.querySelector("#play");
-const powerButton = document.querySelector('#power')
+const powerButton = document.querySelector("#power");
 const resetButton = document.querySelector("#reset");
 const difficultyButton = document.querySelector("#hardmode");
-let order = [topLeft, topRight, bottomLeft, bottomRight];
 
-powerButton.addEventListener('click', (e) => {
-if(powerButton.checked == true) {
+// this notifies the player
+powerButton.addEventListener("click", (e) => {
+  if (powerButton.checked == true) {
     power = true;
     streakCounter.innerHTML = "-";
-    alert('Power is on!');
-}else {
-    if(powerButton.checked == false)
-    streakCounter.innerHTML = '';
+    alert("Power is on!");
+  } else {
+    if (powerButton.checked == false) streakCounter.innerHTML = "";
     // clearAll();
     // clearInterval(intervalId);
     alert("Powering down :(");
-}
-})
-startButton.addEventListener('click', (e) => {
-    if(power||win) {
+  }
+});
+// this starts the game after the player hits the "power checkbox"
+startButton.addEventListener("click", (e) => {
+    if (power || win) {
         gameOn();
-        compTurn(randomColor)
+        compTurn();
+        allColors.forEach((color) => {
+          color.addEventListener("click", function (e) {
+            console.log("reached");
+            playerTurn.push(e.target);
+            console.log(e.target);
+            console.log(playerTurn);
+          });
+        });
+        checkColor();
+        // playerClick();
     }
-})
+});
 
 function gameOn() {
-    win = false;
-    blink = 0;
-    order = [topLeft, topRight, bottomLeft, bottomRight];
-    playerTurn = [];
-    intervalId = 0;
-    turn = 1;
-    streakCounter.innerHTML = 1;
-
+  win = false;
+  order = [topLeft, topRight, bottomLeft, bottomRight];
+  playerTurn = [];
+  turn = 1;
+  streakCounter.innerHTML = 1;
 }
-let randomColor = Math.floor(Math.random() * order.length)
+// this is to notify the player of the next level after completing the sequence
+function newRound() {
+  level += 1;
+  console.log("Can you beat this level?");
+}
+// this needs to randomize the colors
+function randomBlink() {
+  let order = [topLeft, topRight, bottomLeft, bottomRight];
+  let randomColor = Math.floor(Math.random() * order.length);
+  return randomColor;
+}
+// computer makes random colors
 function compTurn() {
-    topLeft.classList.add('active')
-    setTimeout(() => {
-        topLeft.classList.remove('active');
-        topRight.classList.add('active')
-        setTimeout(() => {
+  let randomNum = randomBlink();
 
-            topRight.classList.remove('active')
-            bottomRight.classList.add('active')
-        }, 1500)
-        bottomRight.classList.remove('active')
-    }, 1000)
+  order[randomNum].classList.add("active");
+  compBlink.push(order[randomNum]);
+  console.log(compBlink);
+  setTimeout(() => {
+    order[randomNum].classList.remove("active");
+  }, 1000);
+  //   setTimeout(() =>{
+
+  //   },2000)
+  //   playerClick();
+}
+// Checking player input  to computer sequence before next round
+function checkColor() {
+  if (playerTurn === compTurn) {
+    console.log("Good job!");
+  } else {
+    console.log("FAIL");
+  }
 }
 
