938f4b1efe23753eafbe5122ccae2013ae3f069f
bwennin92 <bwennin92@gmail.com>
2023-05-31 20:22:45 -0700

    made eventlistener for play button

--- script.js
+++ script.js
@@ -1,74 +1,56 @@
-// let playerTurn = [];
-// let win;
-// let power = false;
-// let compBlink = [];
-let order = [];
+const startButton = document.querySelector("#play");
+// 👇 this is used in the main execution loop of the game
+var i = 0
 
+// 👇 this will be the entire history of the simon says
+var sequence = []
 
+// the order var and declaring all of the colored buttons
+// was overcomplicating the problem
+// 👇 this query will get all of the buttons
+const allColors = document.querySelectorAll('.colors')
 
+// 👇 now you can light them up like this
+// allColors[0].classList.add("active")
 
-const allColors = document.querySelectorAll(".colors");
-// const streakCounter = document.querySelector("#turn");
-const blue = document.querySelector("#top_left_button");
-const red = document.querySelector("#top_right_button");
-const green = document.querySelector("#bottom_left_button");
-const yellow = document.querySelector("#bottom_right_button");
-const extCircle = document.querySelector("#extcircle");
-// const playerBox = document.querySelector("#playerbox");
-// const startButton = document.querySelector("#play");
-// const powerButton = document.querySelector("#power");
-// const resetButton = document.querySelector("#reset");
-// const difficultyButton = document.querySelector("#hardmode");
-
-
-// this needs to randomize the colors
-function randomBlink() {
-  let randomOrder = [blue, red, green, yellow];
-  return randomOrder[parseInt(Math.random() * randomOrder.length)];
-};
-// This lets us set the rounds up
-const sequence = [randomBlink()];
-let guessClicks = [...sequence];
-// computer makes random colors
-const compTurn = allColors => {
-  return new Promise((resolve,reject) =>{
-    allColors.className += 'active';
-    setTimeout(() => {
-      allColors.className = allColors.className.replace(
-        'active',
-        ''
-      );
-      setTimeout(() =>{
-        resolve();
-      }, 500);
-    }, 1000);
-  });
-};
-
-//this function lets a random number blink. GREAT, but after the player hits the correct sequence how do i make more numbers blink after the player wins the round?
-
-
-
-let clickRight = false;
-
-const checkColor = checkColor => {
-  if(!clickRight) return;
-  const colorPop = guessClicks.shift();
-  if (colorPop === checkColor) {
-    if (guessClicks.length === 0) {
-      sequence.push(randomBlink());
-      guessClicks = [...sequence];
-    }
-  }else {
-    alert('Sorry try again!')
-  }
-};
-
-const gameOn = async () => {
-  clickRight = false;
-  for (const allColors of sequence){
-    await compTurn(allColors)
-  }
-  clickRight = true;
+function randomQuadrant() {
+  // 👇 keep stuff like this concise
+  return Math.floor(Math.random() * allColors.length)
 }
-gameOn();
+
+function addMove() {
+  // 👇 add random number to the sequence
+  sequence.push(randomQuadrant())
+
+  // 👇 make sure sequence starts at zero
+  i = 0
+}
+
+function compTurn() {
+  // 👇 add "glow" effect to quadrant
+  allColors[sequence[i]].classList.add("active")
+
+  // 👇 remove "glow" effect after x amount of time
+  setTimeout(function () {
+    allColors[sequence[i]].classList.remove("active")
+    i++
+    if (i < sequence.length) {
+      compTurn()
+    }
+    // 👇 sets the delay between blinks in milliseconds
+  }, 500)
+  // 💡 you could also allow the player to specify a time modifier (i.e. slow or fast, easy or hard)
+}
+
+// 👇 add a random number to the seq
+addMove()
+
+// 👇 start main execution loop
+compTurn()
+
+
+  startButton.addEventListener("click", () =>{
+addMove()
+compTurn()
+  })
+
